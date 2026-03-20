2c6d3ac6b730570c394752d00ba75d40bacee7b5
xionter <andrey0502255@gmail.com>
2026-03-20 11:32:22 +0500

    IO works. TODO: simple rest api

--- main.go
+++ main.go
@@ -3,49 +3,73 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/chromedp/chromedp"
 	"regexp"
 	"sync"
+
+	"github.com/chromedp/chromedp"
+
+	//	"sync"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func unique(arr []string) []string {
-	var unique []string
-	for i := 0; i < len(arr); i += 2 {
-		unique = append(unique, arr[i])
-	}
-	return unique
-}
+func main() {
 
-func handleProblem(parent context.Context, problem string) {
-	ctx, cancel := chromedp.NewContext(parent)
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
+		chromedp.Flag("enable-automation", false),
+		chromedp.Flag("disable-blink-features", "AutomationControlled"))
+
+	allocCtx, alocCancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer alocCancel()
+	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
-	var inputs []string
-	var outputs []string
+	url := "https://codeforces.com/group/lwQWQuob0B/contests"
 
-	err := chromedp.Run(ctx, chromedp.Navigate(problem),
-		chromedp.WaitReady("div.input"),
+	var contestURL []string
+	var contestNames []string
+	var contestRemainingTime []string
+	err := chromedp.Run(ctx, chromedp.Navigate(url),
+				chromedp.WaitReady("tr[data-groupcontestid]"),
+				chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('tr[data-groupcontestid]'))
+				.map(contest => contest.children[0].children[1].href)
+					`, &contestURL),
 
-		chromedp.Evaluate(`
-				Array.from(document.querySelectorAll("div.input"))
-				.map((title) => title.children[1].innerText)
-				`, &inputs),
-
-		chromedp.WaitReady("div.output"),
-		chromedp.Evaluate(`
-				Array.from(document.querySelectorAll("div.output"))
-				.map((title) => title.children[1].innerText)
-				`, &outputs))
+				chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('tr[data-groupcontestid]')).
+				map(contest => contest.children[0].firstChild.data)	
+				`, &contestNames),
+				chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('tr[data-groupcontestid]'))
+				.map(contest => contest.children[3]?.children[2])
+				.map(standings => standings?.children[0]?.children[0]?.title)	
+				`, &contestRemainingTime))
 
 	if err != nil {
-		fmt.Println("coulndt handle contest input", err)
+		fmt.Printf("couldn't visit given url: %v and extract contests %v", url, err)
 		return
 	}
-
-	fmt.Println(">Task")
-	fmt.Println(inputs)
-	fmt.Println(outputs)
-	fmt.Println("End Task<")
+	n := len(contestNames)
+	for i := range n {
+		fmt.Printf("%v) %v ", i + 1, strings.TrimSpace(contestNames[i]))
+		if contestRemainingTime[i] != "" {
+			fmt.Printf("(remaining time: %v)", contestRemainingTime[i])
+		}
+		fmt.Println()
+	}
+	
+	fmt.Printf("Specify contest number to scrape(1 - %v):\n", n)
+	var pick int
+	_, err = fmt.Scan(&pick)
+	pick--
+	if err != nil {
+		fmt.Printf("Plese provide valid number. err: %v", err)
+		return
+	}
+	handleContest(ctx, contestURL[pick])
 }
 
 func handleContest(parent context.Context, contest string) {
@@ -71,47 +95,82 @@ func handleContest(parent context.Context, contest string) {
 			problems = append(problems, problem)
 		}
 	}
-	problems = unique(problems)
-	for _, problem := range problems {
-		handleProblem(ctx, problem)
+	problems = makeUnique(problems)
+	var wg sync.WaitGroup
+	for i, problem := range problems {
+		wg.Go(func() {
+			handleProblem(fmt.Sprintf("problem%d", i + 1), ctx, problem)
+		})
 	}
+	wg.Wait()
 }
 
-func main() {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
-		chromedp.Flag("enable-automation", false),
-		chromedp.Flag("disable-blink-features", "AutomationControlled"))
-
-	allocCtx, alocCancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer alocCancel()
-	ctx, cancel := chromedp.NewContext(allocCtx)
+func handleProblem(name string, parent context.Context, problem string) {
+	ctx, cancel := chromedp.NewContext(parent)
 	defer cancel()
 
-	url := "https://codeforces.com/group/lwQWQuob0B/contests"
+	var inputs []string
+	var outputs []string
 
-	var urls []string
-	err := chromedp.Run(ctx, chromedp.Navigate(url),
-		chromedp.WaitReady(".contests-table.group-contests-container"),
+	err := chromedp.Run(ctx, chromedp.Navigate(problem),
+		chromedp.WaitReady("div.input"),
+
 		chromedp.Evaluate(`
-			Array.from(document.querySelectorAll("a"))
-			.map(a => a.href)
-			`, &urls))
+				Array.from(document.querySelectorAll("div.input"))
+				.map((title) => title.children[1].innerText)
+				`, &inputs),
+
+		chromedp.WaitReady("div.output"),
+		chromedp.Evaluate(`
+				Array.from(document.querySelectorAll("div.output"))
+				.map((title) => title.children[1].innerText)
+				`, &outputs))
 
 	if err != nil {
-		fmt.Printf("couldn't visit given url: %v and extract contests", url)
+		fmt.Println("coulnd't handle contest IO", err)
+		return
+	}
+	writeData(name, inputs, outputs)
+}
+
+func writeData(name string, input []string, output []string) {
+	folderPath := filepath.Join("tests", name)
+	err := os.MkdirAll(folderPath, 0755)
+	if err != nil {
+		fmt.Println("couldn't create a tests directory")
 		return
 	}
 
-	contestPattern := `/contest/\d+/?$`
-	var wg sync.WaitGroup
-	for _, url := range urls {
-		match, _ := regexp.MatchString(contestPattern, url)
-		if match {
-			wg.Go(func() {
-				handleContest(ctx, url)
-			})
+	for i := range len(input) {
+		path := filepath.Join(folderPath, fmt.Sprintf("input%d", i + 1))
+		f, err := os.Create(path)
+		if err != nil {
+			fmt.Printf("couldn't create file %v \n", path)
 		}
+		f.WriteString(input[i])
+		f.Close()
+		path = filepath.Join(folderPath, fmt.Sprintf("output%d", i + 1))
+		f, err = os.Create(path)
+		if err != nil {
+			fmt.Printf("couldn't create file %v \n", path)
+		}
+		f.WriteString(output[i])
+		f.Close()
 	}
-	wg.Wait()
+}
+
+func makeUnique(arr []string) []string {
+	var unique []string
+	for i := 0; i < len(arr); i += 2 {
+		unique = append(unique, arr[i])
+	}
+	return unique
+}
+
+func folderExists(path string) bool {
+	status, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return status.IsDir()
 }
